b52097ac8be8c54987df248d68f314cb47c11637
João Aroeira <joao_aroeira@Mac-mini-de-Joao.local>
2020-04-21 15:08:48 -0300

    fixed flag images and added section your location

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,11 @@
 import 'package:covid19_info/core/viewmodels/world_cases_list_view_model.dart';
 import 'package:get_it/get_it.dart';
 
+import 'core/services/location_service.dart';
+
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => WorldCasesListViewModel());
+  locator.registerLazySingleton(() => LocationService());
 }
